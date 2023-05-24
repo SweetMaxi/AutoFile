e7bcb72eb27718b60c74b99ea81cb4aac2b29c1c
SweetMaxi <84555729+SweetMaxi@users.noreply.github.com>
2023-05-24 23:13:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -192,7 +192,7 @@ async def start(client, message):
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(mention=message.from_user.mention, file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
+                    f_caption=CUSTOM_FILE_CAPTION.format(previouscaption= '' if title is None else title, file_size='' if size is None else size, file_caption='')
 
                 except:
 
@@ -212,7 +212,7 @@ async def start(client, message):
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+            f_caption=CUSTOM_FILE_CAPTION.format(previouscaption= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
