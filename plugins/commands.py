b6e147c10b3467b6d700998fbb81076eba6553a4
SweetMaxi <84555729+SweetMaxi@users.noreply.github.com>
2023-05-24 22:36:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -193,7 +193,17 @@ async def start(client, message):
             if CUSTOM_FILE_CAPTION:
                 try:
                     f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
+
                 except:
+
+                    return
+
+            await msg.edit_caption(f_caption)
+
+            return
+
+        except:
+
             pass
         return await message.reply('Name Error')
     files = files_[0]
